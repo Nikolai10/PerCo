598cb0cb018638446ba2c0b7d7b4bc8d93e53a82
Nikolai10 <nikkoerber@gmail.com>
2024-06-16 23:00:31 +0200

    updated README

--- src/train_sd_perco.py
+++ src/train_sd_perco.py
@@ -732,6 +732,7 @@ def main():
     else:
         optimizer_cls = torch.optim.AdamW
 
+    '''
     # Get the names of all linear layers
     linear_layer_names = find_linear_layers(unet)
     # Get the parameters of all linear layers
@@ -745,8 +746,9 @@ def main():
     print(f"Percentage of Linear Layer Parameters: {percentage_linear_params:.2f}%")
 
     trainable_parameters = linear_params + list(hyper_enc.parameters())
+    '''
 
-    # trainable_parameters = list(unet.parameters()) + list(hyper_enc.parameters())
+    trainable_parameters = list(unet.parameters()) + list(hyper_enc.parameters())
 
     optimizer = optimizer_cls(
         trainable_parameters,
